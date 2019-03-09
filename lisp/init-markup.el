184d9df75279048ae8c101d719afc70f1295efc3
Yilin Zhang <yzhang0129@outlook.com>
2019-03-09 23:22:45 +0800

    Add gnuplot-mode.

--- lisp/init-markup.el
+++ lisp/init-markup.el
@@ -42,13 +42,24 @@
 ;; --------------------------------------------------------------
 ;;                    LilyPond Mode Configurations
 ;; --------------------------------------------------------------
-
+;; This package is in the system directory: /usr/share/emacs/site-lisp
 (autoload 'LilyPond-mode "lilypond-mode" "LilyPond Editing Mode" t)
 (add-to-list 'auto-mode-alist '("\\.ly$" . LilyPond-mode))
 (add-to-list 'auto-mode-alist '("\\.ily$" . LilyPond-mode))
 
 (setq LilyPond-pdf-command "atril")
 
+;; --------------------------------------------------------------
+;;                     Gnuplot Mode Configurations
+;; --------------------------------------------------------------
+(use-package gnuplot-mode
+  :ensure t
+  :config
+  ;; specify the gnuplot executable (if other than /usr/bin/gnuplot)
+  ;; (setq gnuplot-program "/sw/bin/gnuplot")
+  ;; automatically open files ending with .gp or .gnuplot in gnuplot mode
+  (setq auto-mode-alist
+	(append '(("\\.\\(gp\\|gnuplot\\)$" . gnuplot-mode)) auto-mode-alist)))
 
 (provide 'init-markup)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
--- lisp/init-org.el
+++ lisp/init-org.el
@@ -2,6 +2,9 @@
 
 (require 'org)
 
+;; --------------------------------------------------------------
+;;                            Features
+;; --------------------------------------------------------------
 ;; add time stamp after an item is DONE
 (setq org-log-done 'time)
 
@@ -17,19 +20,43 @@
 ;; makes special character format visible
 (setq org-pretty-entities t)
 
+;; set org-indent-mode
+(add-hook 'org-mode-hook 'org-indent-mode)
+(add-hook 'org-mode-hook '(lambda()(diminish 'org-indent-mode)))
+
+;; automatically return
+(add-hook 'org-mode-hook
+	  (lambda()
+	    (setq truncate-lines nil)))
+
+(use-package org-bullets
+  :ensure t
+  :hook (org-mode . (lambda () (org-bullets-mode 1))))
+
+;; --------------------------------------------------------------
+;;                 Markdown, LaTeX and Export
+;; --------------------------------------------------------------
+;; Latex preview scale
+(setq org-format-latex-options (plist-put org-format-latex-options :scale 2.3))
+
 ;; suppport export markdown
 ;; original: (ascii html icalendar latex)
 (setq org-export-backends
       (quote (ascii html icalendar latex md)))
 
-(setq org-todo-keywords '((sequence "TODO(t)" "DOING(i)" "HANGUP(h)"
-				    "|" "DONE(d)" "CANCEL(c)")))
-;; set org-indent-mode
-(add-hook 'org-mode-hook 'org-indent-mode)
-(add-hook 'org-mode-hook '(lambda()(diminish 'org-indent-mode)))
+;; Set XeLaTeX as the default LaTeX compiler
+(setq org-latex-compiler "xelatex")
 
-;; Org agenda settings
+;; ox-hugo is an Org exporter backend that exports Org to Hugo-compatible
+;; Markdown (Blackfriday) and also generates the front-matter
+;; (in TOML or YAML format).
+(use-package ox-hugo
+  :ensure t
+  :after ox)
 
+;; --------------------------------------------------------------
+;;                            Agenda
+;; --------------------------------------------------------------
 ;; Set my org agenda file
 ;; '(org-agenda-files (quote ("~/todolist.org")))
 (setq org-agenda-files '("~/agenda.org"))
@@ -38,27 +65,14 @@
 (global-set-key (kbd "C-c a") 'org-agenda)
 (global-set-key (kbd "C-c b") 'org-switchb)
 
-;; Latex preview scale
-(setq org-format-latex-options (plist-put org-format-latex-options :scale 2.3))
+(setq org-todo-keywords '((sequence "TODO(t)" "DOING(i)" "HANGUP(h)"
+				    "|" "DONE(d)" "CANCEL(c)")))
 
-;; Set XeLaTeX as the default LaTeX compiler
-(setq org-latex-compiler "xelatex")
-
-(use-package org-bullets
-  :ensure t
-  :hook (org-mode . (lambda () (org-bullets-mode 1))))
-
-;; automatically return
-(add-hook 'org-mode-hook
-	  (lambda()
-	    (setq truncate-lines nil)))
-
-;; ox-hugo is an Org exporter backend that exports Org to Hugo-compatible
-;; Markdown (Blackfriday) and also generates the front-matter
-;; (in TOML or YAML format).
-(use-package ox-hugo
-  :ensure t
-  :after ox)
+;; --------------------------------------------------------------
+;;                            Capture
+;; --------------------------------------------------------------
+(global-set-key (kbd "C-c c") 'org-capture)
+(setq org-default-notes-file "~/inbox.org")
 
 (provide 'init-org)
 

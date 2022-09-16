a59e7e530380fa168696dcfa6756757be79459fb
Yilin Zhang <yzhang0129@outlook.com>
2022-09-16 17:39:32 -0400

    disable citre's auto-enable (fix)

--- lisp/init-prog.el
+++ lisp/init-prog.el
@@ -60,8 +60,7 @@
    ;; By default, when you open any file, and a tags file can be found for it,
    ;; `citre-mode' is automatically enabled.  If you only want this to work for
    ;; certain modes (like `prog-mode'), set it like this.
-   ;; citre-auto-enable-citre-mode-modes '(prog-mode))
-   ))
+   citre-auto-enable-citre-mode-modes nil))
 
 ;; --------------------------------------------------------------
 ;;                     C/C++ Mode Configurations

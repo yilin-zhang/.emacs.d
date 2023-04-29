3863de111e7f1df2029c0e69532618ed4e5f5e4e
Yilin Zhang <yzhang0129@outlook.com>
2023-04-28 23:38:23 -0700

    remove symbol-overlay

--- lisp/init-edit.el
+++ lisp/init-edit.el
@@ -575,36 +575,6 @@
 ;; --------------------------------------------------------------
 ;;                            Hightlight
 ;; --------------------------------------------------------------
-;; Highlight symbols
-(use-package symbol-overlay
-  :diminish
-  :defines iedit-mode
-  :bind (("M-i" . symbol-overlay-put)
-         ("M-n" . symbol-overlay-jump-next)
-         ("M-p" . symbol-overlay-jump-prev)
-         ("M-N" . symbol-overlay-switch-forward)
-         ("M-P" . symbol-overlay-switch-backward)
-         ("M-C" . symbol-overlay-remove-all)
-         ([M-f3] . symbol-overlay-remove-all))
-  :hook ((prog-mode . symbol-overlay-mode)
-         (iedit-mode . (lambda () (symbol-overlay-mode -1)))
-         (iedit-mode-end . symbol-overlay-mode))
-  :config
-  ;; Disable symbol highlighting while selecting
-  (defun turn-off-symbol-overlay (&rest _)
-    "Turn off symbol highlighting."
-    (interactive)
-    (symbol-overlay-mode -1))
-  (advice-add #'set-mark :after #'turn-off-symbol-overlay)
-
-  (defun turn-on-symbol-overlay (&rest _)
-    "Turn on symbol highlighting."
-    (interactive)
-    (when (derived-mode-p 'prog-mode 'yaml-mode)
-      (symbol-overlay-mode 1)))
-  (advice-add #'deactivate-mark :after #'turn-on-symbol-overlay))
-
-
 ;; highlight indentations
 (use-package highlight-indent-guides
   :diminish

0c6aed686beab23237c5cd06c671268ca7c4af6e
Yilin Zhang <yzhang0129@outlook.com>
2023-05-06 20:22:50 -0700

    disable meow in magit

--- lisp/init-git.el
+++ lisp/init-git.el
@@ -4,6 +4,7 @@
 (use-package magit
   :commands (transient-insert-suffix magit-status)
   :bind ("C-x g" . magit-status)
+  :hook (magit-mode . (lambda () (meow-mode -1)))
   ;; turn off auto revert since global-auto-revert-mode is enabled
   :config (magit-auto-revert-mode -1))
 

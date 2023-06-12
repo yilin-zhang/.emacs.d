f829ba40fe46d744fb7626f567e8b4499f04fd34
Yilin Zhang <yzhang0129@outlook.com>
2023-06-11 19:09:42 -0700

    organize basic configs

--- lisp/init-basic.el
+++ lisp/init-basic.el
@@ -8,15 +8,6 @@
 ;; https://emacs-lsp.github.io/lsp-mode/page/performance/
 (setq read-process-output-max (* 3 1024 1024))
 
-(defun yilin/display-startup-time ()
-  (message "Emacs loaded in %s with %d garbage collections."
-           (format "%.2f seconds"
-                   (float-time
-                    (time-subtract after-init-time before-init-time)))
-           gcs-done))
-
-(add-hook 'emacs-startup-hook #'yilin/display-startup-time)
-
 ;; settings from Centaur Emcas
 (use-package gcmh
   :hook after-init
@@ -50,11 +41,18 @@
   :init
   (tool-bar-mode -1)
   (setq inhibit-splash-screen 1) ; disable welcome screen
+  ;; The setting of this variable must come before enable
+  ;; display-time-mode, or it will not work.
+  (setq display-time-24hr-format 1
+        display-time-string-forms
+        '((propertize (concat 24-hours ":" minutes " ")
+                      'face 'font-lock-constant-face)))
   :hook
   (after-init . global-so-long-mode)
   (after-init . delete-selection-mode)
   (after-init . global-hl-line-mode) ; highlight the current line
   (after-init . pixel-scroll-precision-mode)
+  (emacs-startup . yilin/display-startup-time)
   :config
   ;; Confirmation
   (fset 'yes-or-no-p 'y-or-n-p) ; change yes or no to y or n
@@ -75,10 +73,59 @@
   (setq make-backup-files nil  ; disable backup file
         auto-save-default nil  ; disable auto-save
         )
+
+  ;; Full Screen with Mode Line Time Display
+  (defun yilin/toggle-frame-fullscreen ()
+    "toggle-frame-fullscreen plus display-time-mode."
+    (interactive)
+    (if (equal 'fullboth (frame-parameter nil 'fullscreen))
+        (progn
+          (display-time-mode -1)
+          (display-battery-mode -1))
+      (progn
+        (display-time-mode 1)
+        (display-battery-mode 1)))
+    (toggle-frame-fullscreen))
+
+  ;; Set key-binding for switching from a horizontal
+  ;; split to a vertical split and vice versa.
+  (defun yilin/toggle-window-split ()
+    (interactive)
+    (if (= (count-windows) 2)
+        (let* ((this-win-buffer (window-buffer))
+               (next-win-buffer (window-buffer (next-window)))
+               (this-win-edges (window-edges (selected-window)))
+               (next-win-edges (window-edges (next-window)))
+               (this-win-2nd (not (and (<= (car this-win-edges)
+                                           (car next-win-edges))
+                                       (<= (cadr this-win-edges)
+                                           (cadr next-win-edges)))))
+               (splitter
+                (if (= (car this-win-edges)
+                       (car (window-edges (next-window))))
+                    'split-window-horizontally
+                  'split-window-vertically)))
+          (delete-other-windows)
+          (let ((first-win (selected-window)))
+            (funcall splitter)
+            (if this-win-2nd (other-window 1))
+            (set-window-buffer (selected-window) this-win-buffer)
+            (set-window-buffer (next-window) next-win-buffer)
+            (select-window first-win)
+            (if this-win-2nd (other-window 1))))))
+
+  (defun yilin/display-startup-time ()
+    (message "Emacs loaded in %s with %d garbage collections."
+             (format "%.2f seconds"
+                     (float-time
+                      (time-subtract after-init-time before-init-time)))
+             gcs-done))
   :bind
   ;; Focus on the new window after splitting
   ("C-x 2" . (lambda () (interactive) (split-window-vertically) (other-window 1)))
   ("C-x 3" . (lambda () (interactive) (split-window-horizontally) (other-window 1)))
+  ("<f12>" . yilin/toggle-frame-fullscreen)
+  ("C-x |" . yilin/toggle-window-split)
   )
 
 (use-package popwin
@@ -218,80 +265,6 @@
   (doom-modeline-modal-icon nil)
   (find-file-visit-truename t))
 
-;; --------------------------------------------------------------
-;;                            Custom
-;; --------------------------------------------------------------
-
-;; Full Screen with Mode Line Time Display
-
-;; The setting of this variable must come before enable
-;; display-time-mode, or it will not work.
-(setq display-time-24hr-format 1
-      display-time-string-forms
-      '((propertize (concat 24-hours ":" minutes " ")
-                    'face 'font-lock-constant-face)))
-
-
-(defun yilin/toggle-frame-fullscreen ()
-  "toggle-frame-fullscreen plus display-time-mode."
-  (interactive)
-  (if (equal 'fullboth (frame-parameter nil 'fullscreen))
-      (progn
-        (display-time-mode -1)
-        (display-battery-mode -1))
-    (progn
-      (display-time-mode 1)
-      (display-battery-mode 1)))
-  (toggle-frame-fullscreen))
-
-(global-set-key (kbd "<f12>") 'yilin/toggle-frame-fullscreen)
-;; (yilin-toggle-frame-fullscreen)
-
-;; Window/Frame
-
-;; Set key-binding for switching from a horizontal
-;; split to a vertical split and vice versa.
-(defun yilin/toggle-window-split ()
-  (interactive)
-  (if (= (count-windows) 2)
-      (let* ((this-win-buffer (window-buffer))
-             (next-win-buffer (window-buffer (next-window)))
-             (this-win-edges (window-edges (selected-window)))
-             (next-win-edges (window-edges (next-window)))
-             (this-win-2nd (not (and (<= (car this-win-edges)
-                                         (car next-win-edges))
-                                     (<= (cadr this-win-edges)
-                                         (cadr next-win-edges)))))
-             (splitter
-              (if (= (car this-win-edges)
-                     (car (window-edges (next-window))))
-                  'split-window-horizontally
-                'split-window-vertically)))
-        (delete-other-windows)
-        (let ((first-win (selected-window)))
-          (funcall splitter)
-          (if this-win-2nd (other-window 1))
-          (set-window-buffer (selected-window) this-win-buffer)
-          (set-window-buffer (next-window) next-win-buffer)
-          (select-window first-win)
-          (if this-win-2nd (other-window 1))))))
-
-(global-set-key (kbd "C-x |") 'yilin/toggle-window-split)
-
-;; --------------------------------------------------------------
-;;                            Backup
-;; --------------------------------------------------------------
-;; inhibit the start message
-;; (setq inhibit-startup-message t)
-
-;; save desktop
-;; (desktop-save-mode t)
-
-;; press <f2> to open my init.el file
-;;(defun open-init-file()
-;;  (interactive)
-;;  (find-file "~/.emacs.d/init.el"))
-;;(global-set-key (kbd "<f2>") 'open-init-file)
 
 (provide 'init-basic)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;

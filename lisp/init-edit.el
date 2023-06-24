d375b4afdad21dd213055886c5445d57d3575ce4
Yilin Zhang <yzhang0129@outlook.com>
2023-06-23 20:02:46 -0700

    use completion category defaults

--- lisp/init-edit.el
+++ lisp/init-edit.el
@@ -353,7 +353,6 @@
 (use-package orderless
   :custom
   (completion-styles '(orderless basic))
-  (completion-category-defaults nil)
   (completion-category-overrides '((file (styles partial-completion)))))
 
 (use-package marginalia
